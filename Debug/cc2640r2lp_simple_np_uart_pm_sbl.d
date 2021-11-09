4efa79a4f0131ff6b36960ae1722755d97e1f38c
zichao <zichao2018@gmail.com>
2021-11-09 13:22:33 -0500

    delete debug

--- Debug/cc2640r2lp_simple_np_uart_pm_sbl.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-cc2640r2lp_simple_np_uart_pm_sbl.obj: C:/ti/simplelink_sdk_ble_plugin_3_20_00_24/source/ti/snp/blestack/c-array/cc2640r2lp_simple_np_uart_pm_sbl.c
-
-C:/ti/simplelink_sdk_ble_plugin_3_20_00_24/source/ti/snp/blestack/c-array/cc2640r2lp_simple_np_uart_pm_sbl.c: 
